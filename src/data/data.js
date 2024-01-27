3ab3abfaa699f9db97416c3347eba9ff394909f2
Mateus <mateusc.ramos@outlook.com>
2024-01-27 18:12:54 -0300

    feat: base data script

---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+const data = [
+  {
+    min: 0,
+    max: 18.4,
+    range: "Menor que 18,5",
+    classification: "Abaixo do peso",
+    obesityLevel: "0",
+    class: "underweight",
+  },
+  {
+    min: 18.5,
+    max: 24.9,
+    range: "Entre 18,5 e 24,9",
+    classification: "Normal",
+    obesityLevel: "0",
+    class: "normal",
+  },
+  {
+    min: 25,
+    max: 29.9,
+    range: "Entre 25,0 e 29,9",
+    classification: "Sobrepeso",
+    obesityLevel: "I",
+    class: "overweight",
+  },
+  {
+    min: 30,
+    max: 39.9,
+    range: "Entre 30,0 e 39,9",
+    classification: "Obesidade",
+    obesityLevel: "II",
+    class: "obesity",
+  },
+  {
+    min: 40,
+    max: 99,
+    range: "Maior que 40,0",
+    classification: "Obesidade grave",
+    obesityLevel: "III",
+    class: "severe-obesity",
+  },
+];
